fad3fc02004b6fd74034fd49b3c95ea975f5c858
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-20 22:07:00 +0700

    Add files via upload

---
+++ ling/__main__.py
@@ -0,0 +1,57 @@
+import time
+import importlib 
+from pyrogram import idle
+from uvloop import install
+
+from config import BOT_VER, CMD_HANDLER
+from rams import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots, app, ids
+from rams.split.misc import create_botlog, git, heroku
+from rams.modules import ALL_MODULES
+
+MSG_ON = """
+🔥 **RamPyro-Bot Menyala** 🔥
+╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
+🤖 **Userbot Version -** `{}`
+⌨️ **Ketik** `{}rama` **untuk Mengecheck Bot**
+╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
+"""
+MSG_BOT = (f"**Ram Pyro Assistant**\nis alive...")
+
+
+async def main():
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module(f"rams.modules.{all_module}")
+    for bot in bots:
+        try:
+            await bot.start()
+            bot.me = await bot.get_me()
+            await bot.join_chat("geezram")
+            await bot.join_chat("userbotch")
+            await bot.join_chat("b4c0d")
+            await bot.join_chat("teman_random")
+            ids.append(bot.me.id)
+            try:
+                await bot.send_message(
+                    BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER)
+                )
+                await app.send_message(BOTLOG_CHATID, MSG_BOT)
+            except BaseException:
+                pass
+            LOGGER("rams").info(
+                f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
+            )
+        except Exception as a:
+            LOGGER("master").warning(a)
+    LOGGER("rams").info(f"RamPyro-Bot v{BOT_VER} [🔥 UDAH AKTIF NGENTOT! 🔥]")
+    if not str(BOTLOG_CHATID).startswith("-100"):
+        await create_botlog(bot1)
+    await idle()
+    await aiosession.close()
+
+
+if __name__ == "__main__":
+    LOGGER("rams").info("Starting RamPyro-Bot")
+    install()
+    heroku()
+    LOOP.run_until_complete(main())
