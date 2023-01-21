a0f7e39d2d230c5a0df9395e91171a7d5e542afb
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-21 19:40:38 +0700

    Update help.py

--- ling/modules/help.py
+++ ling/modules/help.py
@@ -13,10 +13,10 @@ import asyncio
 from prettytable import PrettyTable
 from pyrogram import Client, enums, filters
 from pyrogram.types import Message
-from PyroKar.helpers.PyroHelpers import ReplyCheck
-from PyroKar.helpers.utility import split_list
-from PyroKar import app, CMD_HELP
-from PyroKar.helpers.cmd import *
+from ling.helpers.PyroHelpers import ReplyCheck
+from ling.helpers.utility import split_list
+from ling import app, CMD_HELP
+from ling.helpers.cmd import *
 
 async def edit_or_reply(message: Message, *args, **kwargs) -> Message:
     xyz = (
@@ -46,13 +46,13 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "PyroKar-UserBot Modules"
+            ac.title = "HyperRobot Modules"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @Karc0de >< @obrolansuar •",
+                f"```{str(ac)}```\n• @storyQi >< @HyperSupportQ •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -66,7 +66,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"**Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `*{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @Karc0de"
+            this_command += "© @storyQi"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -88,12 +88,12 @@ async def module_helper(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "PyroKar-UserBot Modules"
+        ac.title = "HyperRobot Modules"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @Karc0de >< @obrolansuar •"
+            message, f"```{str(ac)}```\n• @storyQi >< @HyperSupportQ •"
         )
         await message.reply(
             f"**Usage**:`*help broadcast` **To View Module details**"
@@ -105,7 +105,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `*{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @Karc0de"
+            this_command += "© @storyQi"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
