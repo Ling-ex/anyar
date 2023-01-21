c6bf7f04540ece8de31dfc5668664e70ac3fa5de
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-21 13:57:27 +0700

    Delete hyperlibs/ling directory

--- hyperlibs/ling/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from hyperlibs.ling.utils.misc import *
-from hyperlibs.ling.utils.pastebin import *
-from hyperlibs.ling.utils.sections import *
-from hyperlibs.ling.utils.tools import *
-from hyperlibs.ling.utils._adzan import __fetch_adzan
