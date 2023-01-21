2dc3fa5fc245bbed700d25ff17a4ce5350a56fc7
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-21 15:07:00 +0700

    Update __init__.py

--- ling/helpers/SQL/__init__.py
+++ ling/helpers/SQL/__init__.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from rams import DB_URL, LOGGER
+from ling import DB_URL, LOGGER
 
 SPAMBOT = "SPAMBOT"
 
