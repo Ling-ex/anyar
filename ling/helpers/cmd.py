d9c1abb2a890470d071e2729aa6e3b639b9f5108
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-21 20:28:15 +0700

    Add files via upload

---
+++ ling/helpers/cmd.py
@@ -0,0 +1,10 @@
+from pyrogram import *
+from pyrogram import Client as gez
+
+PREFIX = ["^", "?", "-", "+"]
+
+cmd = [".", "*", ",,", "-"] # cmd custom
+
+command = filters.command
+
+dont_know = -1001640257827
