61ca53ccc453329e5ddf93b51b5671a1c168e97f
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-21 16:11:03 +0700

    Update stickers.py

--- ling/modules/stickers.py
+++ ling/modules/stickers.py
@@ -22,10 +22,10 @@ from pyrogram.errors import StickersetInvalid, YouBlockedUser
 from pyrogram.raw.functions.messages import GetStickerSet
 from pyrogram.raw.types import InputStickerSetShortName
 from pyrogram.types import Message
-from hyperlibs.ling.helpers.basic import edit_or_reply
-from hyperlibs.ling.helpers.PyroHelpers import ReplyCheck
-from hyperlibs.ling.helpers.tools import get_arg, get_text, resize_media
-from hyperlibs.ling.utils.tools import bash
+from ling.helpers.basic import edit_or_reply
+from ling.helpers.PyroHelpers import ReplyCheck
+from ling.helpers.tools import get_arg, get_text, resize_media
+from ling.utils.tools import bash
 from config import CMD_HANDLER as cmd
 
 from .help import add_command_help
