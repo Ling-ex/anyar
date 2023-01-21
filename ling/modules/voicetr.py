6d4334e9b73124a8d2f6b74ecc940d861b44c192
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-21 16:19:32 +0700

    Update voicetr.py

--- ling/modules/voicetr.py
+++ ling/modules/voicetr.py
@@ -13,7 +13,7 @@ import os
 from gtts import gTTS
 from pyrogram import Client, enums, filters
 from pyrogram.types import Message
-from hyperlibs.ling.helpers.basic import edit_or_reply
+from ling.helpers.basic import edit_or_reply
 from config import CMD_HANDLER as cmd
 
 from .help import add_command_help
