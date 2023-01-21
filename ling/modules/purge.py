b23ecc04ef85333ce9ef50e7acb32f41987a6217
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-21 16:00:20 +0700

    Update purge.py

--- ling/modules/purge.py
+++ ling/modules/purge.py
@@ -11,8 +11,8 @@ import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from hyperlibs.ling.helpers.adminHelpers import DEVS
-from hyperlibs.ling.helpers.basic import edit_or_reply
+from ling.helpers.adminHelpers import DEVS
+from ling.helpers.basic import edit_or_reply
 from config import CMD_HANDLER as cmd
 
 from .help import add_command_help
