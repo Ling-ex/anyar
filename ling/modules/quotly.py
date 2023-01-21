53619f0940a43d90d230100d959b17e1c1fdb47c
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-21 16:00:56 +0700

    Update quotly.py

--- ling/modules/quotly.py
+++ ling/modules/quotly.py
@@ -11,7 +11,7 @@ import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from hyperlibs.ling.helpers.tools import get_arg
+from ling.helpers.tools import get_arg
 from config import CMD_HANDLER as cmd
 
 from .help import add_command_help
