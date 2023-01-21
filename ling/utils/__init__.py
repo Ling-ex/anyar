d325360bf3b1a5c531740c10278adc26be7b7d55
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-21 15:14:54 +0700

    Add files via upload

---
+++ ling/utils/__init__.py
@@ -0,0 +1,5 @@
+from geezlibs.ram.utils.misc import *
+from geezlibs.ram.utils.pastebin import *
+from geezlibs.ram.utils.sections import *
+from geezlibs.ram.utils.tools import *
+from geezlibs.ram.utils._adzan import __fetch_adzan
