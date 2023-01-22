accdbedae5a78adcbed3ef3f2d2f142761165190
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-22 14:21:08 +0700

    Update data.py

--- ling/split/data.py
+++ ling/split/data.py
@@ -4,7 +4,7 @@ from config import CMD_HANDLER as cmd
 class Data:
 
     text_help_menu = (
-        f"**Command List & Help**\n**— Your Command: {cmd}"
+        f"**Command List & Help**\n**Prefix: . - ? ^"
         .replace(",", "")
         .replace("[", "")
         .replace("]", "")
