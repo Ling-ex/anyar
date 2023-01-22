a52360faff4e55b8ed2873023a3574c86525800b
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-22 16:54:04 +0700

    Update data.py

--- ling/helpers/data.py
+++ ling/helpers/data.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardButton, WebAppInfo
 class Data:
 
     text_help_menu = (
-        "**Command & Help\nLu bisa Juga Menggunakan\nPrefix (. ,, - ^ ?)**"
+        "**Command List & Help**\n**Prefix: . ,, - ? ^**"
         .replace(",", "")
         .replace("[", "")
         .replace("]", "")
