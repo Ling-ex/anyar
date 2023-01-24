f2c949c05332c4e82692a03643a30140951bc116
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-24 14:58:17 +0700

    Update misc.py

--- ling/utils/misc.py
+++ ling/utils/misc.py
@@ -7,6 +7,9 @@
 #
 # t.me/SharingUserbot & t.me/Lunatic0de
 
+import socket
+from asyncio import get_running_loop
+from functools import partial
 import os
 import sys
 from re import sub
@@ -17,8 +20,39 @@ from pyrogram import Client, enums
 admins_in_chat = {}
 
 
+def _netcat(host, port, content):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.sendall(content.encode())
+    s.shutdown(socket.SHUT_WR)
+    while True:
+        data = s.recv(4096).decode("utf-8").strip("\n\x00")
+        if not data:
+            break
+        return data
+    s.close()
+
+async def is_heroku():
+    return "heroku" in socket.getfqdn()
+
+
+async def user_input(input):
+    if " " in input or "\n" in input:
+        return str(input.split(maxsplit=1)[1].strip())
+    return ""
+
+async def paste_queue(content):
+    loop = get_running_loop()
+    link = await loop.run_in_executor(
+        None, partial(_netcat, "ezup.dev", 9999, content)
+    )
+    return link
+
+
+
+
 def restart():
-    os.execvp(sys.executable, [sys.executable, "-m", "rams"])
+    os.execvp(sys.executable, [sys.executable, "-m", "ling"])
 
 
 async def list_admins(client: Client, chat_id: int):
