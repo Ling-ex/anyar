582e94eb740f90ed53d5aa7f474381104e25caed
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-24 23:39:23 +0700

    Update inline.py

--- ling/modules/bot/inline.py
+++ ling/modules/bot/inline.py
@@ -97,7 +97,10 @@ async def ping_function(message: Message, answers):
         "  Sᴛᴀᴛᴜꜱ » 𝘚𝘶𝘱𝘦𝘳 𝘗𝘳𝘦𝘮𝘪𝘶𝘮 \n"
         f"    Pɪɴɢᴇʀ ›</b> <code>{duration}ms</code> \n"
         f"    Uᴘᴛɪᴍᴇ ›</b> <code>{uptime}</code> \n"
-        f"   Vᴇʀꜱɪᴏɴ › <code>{BOT_VER}</code> \n"
+        f"    Bᴏᴛ Vᴇʀꜱɪᴏɴ › <code>{BOT_VER}</code> \n"
+        f"    Pʟᴜɢɪɴꜱ ›</b> <code>{len(CMD_HELP)} Modules</code> \n"
+        f"    Pʏᴛʜᴏɴ Vᴇʀꜱɪᴏɴ ›</b> <code>{pyver.split()[0]}</code> \n"
+        f"    Pʏʀᴏɢʀᴀᴍ Vᴇʀꜱɪᴏɴ ›</b> <code>{pyrover}</code> \n"
     )
     answers.append(
         InlineQueryResultArticle(
