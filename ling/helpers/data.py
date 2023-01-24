16595d6a6e36375fbac2a62463a5dccecb9921e6
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-24 22:01:31 +0700

    Update data.py

--- ling/helpers/data.py
+++ ling/helpers/data.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardButton, WebAppInfo
 class Data:
 
     text_help_menu = (
-        "**Help For HyperRobot**\n prefix: . "
+        "**Help for HyperRobot**\n prefix: . "
         .replace(",", "")
         .replace("[", "")
         .replace("]", "")
