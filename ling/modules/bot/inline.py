271dd7590d2655c0b7f925d873f91a540d12d320
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-24 23:46:05 +0700

    Update inline.py

--- ling/modules/bot/inline.py
+++ ling/modules/bot/inline.py
@@ -111,7 +111,7 @@ async def ping_function(message: Message, answers):
                 msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True
             ),
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Support", url="t.me/HyperSupportQ")]]
+                [[InlineKeyboardButton("ᴍᴇɴᴜ ʙᴀɴᴛᴜᴀɴ", callback_data="helper")]]
             ),
         )
     )
