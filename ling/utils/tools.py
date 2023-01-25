522b2cd2b59a20534f0a5ac91b290c0be2fd4b78
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-25 20:08:11 +0700

    Update tools.py

--- ling/utils/tools.py
+++ ling/utils/tools.py
@@ -1,130 +1,187 @@
-# Credits: @mrismanaziz
-# Copyright (C) 2022 Pyro-ManUserbot
-#
-# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
-#
-# t.me/SharingUserbot & t.me/Lunatic0de
-
 import asyncio
+import math
 import os
 import shlex
-import textwrap
 from typing import Tuple
 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
+from pymediainfo import MediaInfo
+from pyrogram.types import Message
 
-kopi = [
-    "**Hadir bang** 😁",
-    "**Hadir kak** 😉",
-    "**Hadir dong** 😁",
-    "**Hadir ganteng** 🥵",
-    "**Hadir bro** 😎",
-    "**Hadir kak maap telat** 🥺",
-]
+import multiprocessing
+import mimetypes
+import functools
+import threading
+from concurrent.futures import ThreadPoolExecutor
 
-
-async def get_readable_time(seconds: int) -> str:
-    count = 0
-    up_time = ""
-    time_list = []
-    time_suffix_list = ["detik", "menit", "Jam", "Hari"]
-
-    while count < 4:
-        count += 1
-        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        up_time += time_list.pop() + ", "
-
-    time_list.reverse()
-    up_time += ", ".join(time_list)
-
-    return up_time
+max_workers = multiprocessing.cpu_count() * 5
+exc_ = ThreadPoolExecutor(max_workers=max_workers)
 
 
-async def add_text_img(image_path, text):
-    font_size = 12
-    stroke_width = 1
-
-    if ";" in text:
-        upper_text, lower_text = text.split(";")
+def get_text(message: Message) -> [None, str]:
+    """Extract Text From Commands"""
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
     else:
-        upper_text = text
-        lower_text = ""
+        return None
 
-    img = Image.open(image_path).convert("RGBA")
-    img_info = img.info
-    image_width, image_height = img.size
-    font = ImageFont.truetype(
-        font="ling/resources/default.ttf",
-        size=int(image_height * font_size) // 100,
+
+def humanbytes(size):
+    """Convert Bytes To Bytes So That Human Can Read It"""
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+def run_in_exc(f):
+    @functools.wraps(f)
+    async def wrapper(*args, **kwargs):
+        loop = asyncio.get_running_loop()
+        return await loop.run_in_executor(exc_, lambda: f(*args, **kwargs))
+    return wrapper
+
+
+def time_formatter(milliseconds: int) -> str:
+    """Time Formatter"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        ((str(days) + " day(s), ") if days else "")
+        + ((str(hours) + " hour(s), ") if hours else "")
+        + ((str(minutes) + " minute(s), ") if minutes else "")
+        + ((str(seconds) + " second(s), ") if seconds else "")
+        + ((str(milliseconds) + " millisecond(s), ") if milliseconds else "")
     )
-    draw = ImageDraw.Draw(img)
-
-    char_width, char_height = font.getsize("A")
-    chars_per_line = image_width // char_width
-    top_lines = textwrap.wrap(upper_text, width=chars_per_line)
-    bottom_lines = textwrap.wrap(lower_text, width=chars_per_line)
-
-    if top_lines:
-        y = 10
-        for line in top_lines:
-            line_width, line_height = font.getsize(line)
-            x = (image_width - line_width) / 2
-            draw.text(
-                (x, y),
-                line,
-                fill="white",
-                font=font,
-                stroke_width=stroke_width,
-                stroke_fill="black",
-            )
-            y += line_height
-
-    if bottom_lines:
-        y = image_height - char_height * len(bottom_lines) - 15
-        for line in bottom_lines:
-            line_width, line_height = font.getsize(line)
-            x = (image_width - line_width) / 2
-            draw.text(
-                (x, y),
-                line,
-                fill="white",
-                font=font,
-                stroke_width=stroke_width,
-                stroke_fill="black",
-            )
-            y += line_height
-
-    final_image = os.path.join("memify.webp")
-    img.save(final_image, **img_info)
-    return final_image
+    return tmp[:-2]
 
 
-# https://github.com/TeamUltroid/pyUltroid/blob/31c271cf4d35ab700e5880e952e54c82046812c2/pyUltroid/functions/helper.py#L154
+async def progress(current, total, message, start, type_of_ps, file_name=None):
+    """Progress Bar For Showing Progress While Uploading / Downloading File - Normal"""
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        if elapsed_time == 0:
+            return
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        progress_str = "{0}{1} {2}%\n".format(
+            "".join(["▰" for i in range(math.floor(percentage / 10))]),
+            "".join(["▱" for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2),
+        )
+        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
+            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
+        )
+        if file_name:
+            try:
+                await message.edit(
+                    "{}\n**File Name:** `{}`\n{}".format(type_of_ps, file_name, tmp)
+                )
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+            except MessageNotModified:
+                pass
+        else:
+            try:
+                await message.edit("{}\n{}".format(type_of_ps, tmp))
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+            except MessageNotModified:
+                pass
 
 
-async def bash(cmd):
-    process = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
+async def cb_progress(current, total, cb, start, type_of_ps, file_name=None):
+    """Progress Bar For Showing Progress While Uploading / Downloading File - Inline"""
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        if elapsed_time == 0:
+            return
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        progress_str = "{0}{1} {2}%\n".format(
+            "".join(["▰" for i in range(math.floor(percentage / 10))]),
+            "".join(["▱" for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2),
+        )
+        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
+            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
+        )
+        if file_name:
+            try:
+                await cb.edit_message_text(
+                    "{}\n**File Name:** `{}`\n{}".format(type_of_ps, file_name, tmp)
+                )
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+            except MessageNotModified:
+                pass
+        else:
+            try:
+                await message.edit_message_text("{}\n{}".format(type_of_ps, tmp))
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+            except MessageNotModified:
+                pass
+
+
+async def edit_or_reply(message: Message, *args, **kwargs) -> Message:
+    apa = (
+        message.edit_text
+        if bool(message.from_user and message.from_user.is_self or message.outgoing)
+        else (message.reply_to_message or message).reply_text
     )
-    stdout, stderr = await process.communicate()
-    err = stderr.decode().strip()
-    out = stdout.decode().strip()
-    return out, err
+    return await apa(*args, **kwargs)
 
 
-async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+def get_arg(message: Message):
+    msg = message.text
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+    split = msg[1:].replace("\n", " \n").split(" ")
+    if " ".join(split[1:]).strip() == "":
+        return ""
+    return " ".join(split[1:])
+
+
+def get_args(message: Message):
+    try:
+        message = message.text
+    except AttributeError:
+        pass
+    if not message:
+        return False
+    message = message.split(maxsplit=1)
+    if len(message) <= 1:
+        return []
+    message = message[1]
+    try:
+        split = shlex.split(message)
+    except ValueError:
+        return message
+    return list(filter(lambda x: len(x) > 0, split))
+
+
+async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
+    """Run Commands"""
     args = shlex.split(cmd)
     process = await asyncio.create_subprocess_exec(
         *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
@@ -136,3 +193,172 @@ async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
         process.returncode,
         process.pid,
     )
+
+
+async def convert_to_image(message, client) -> [None, str]:
+    """Convert Most Media Formats To Raw Image"""
+    if not message:
+        return None
+    if not message.reply_to_message:
+        return None
+    final_path = None
+    if not (
+        message.reply_to_message.video
+        or message.reply_to_message.photo
+        or message.reply_to_message.sticker
+        or message.reply_to_message.media
+        or message.reply_to_message.animation
+        or message.reply_to_message.audio
+    ):
+        return None
+    if message.reply_to_message.photo:
+        final_path = await message.reply_to_message.download()
+    elif message.reply_to_message.sticker:
+        if message.reply_to_message.sticker.mime_type == "image/webp":
+            final_path = "webp_to_png_s_proton.png"
+            path_s = await message.reply_to_message.download()
+            im = Image.open(path_s)
+            im.save(final_path, "PNG")
+        else:
+            path_s = await client.download_media(message.reply_to_message)
+            final_path = "lottie_proton.png"
+            cmd = (
+                f"lottie_convert.py --frame 0 -if lottie -of png {path_s} {final_path}"
+            )
+            await run_cmd(cmd)
+    elif message.reply_to_message.audio:
+        thumb = message.reply_to_message.audio.thumbs[0].file_id
+        final_path = await client.download_media(thumb)
+    elif message.reply_to_message.video or message.reply_to_message.animation:
+        final_path = "fetched_thumb.png"
+        vid_path = await client.download_media(message.reply_to_message)
+        await run_cmd(f"ffmpeg -i {vid_path} -filter:v scale=500:500 -an {final_path}")
+    return final_path
+
+
+def resize_image(image):
+    im = Image.open(image)
+    maxsize = (512, 512)
+    if (im.width and im.height) < 512:
+        size1 = im.width
+        size2 = im.height
+        if im.width > im.height:
+            scale = 512 / size1
+            size1new = 512
+            size2new = size2 * scale
+        else:
+            scale = 512 / size2
+            size1new = size1 * scale
+            size2new = 512
+        size1new = math.floor(size1new)
+        size2new = math.floor(size2new)
+        sizenew = (size1new, size2new)
+        im = im.resize(sizenew)
+    else:
+        im.thumbnail(maxsize)
+    file_name = "Sticker.png"
+    im.save(file_name, "PNG")
+    if os.path.exists(image):
+        os.remove(image)
+    return file_name
+
+
+class Media_Info:
+    def data(media: str) -> dict:
+        "Get downloaded media's information"
+        found = False
+        media_info = MediaInfo.parse(media)
+        for track in media_info.tracks:
+            if track.track_type == "Video":
+                found = True
+                type_ = track.track_type
+                format_ = track.format
+                duration_1 = track.duration
+                other_duration_ = track.other_duration
+                duration_2 = (
+                    f"{other_duration_[0]} - ({other_duration_[3]})"
+                    if other_duration_
+                    else None
+                )
+                pixel_ratio_ = [track.width, track.height]
+                aspect_ratio_1 = track.display_aspect_ratio
+                other_aspect_ratio_ = track.other_display_aspect_ratio
+                aspect_ratio_2 = other_aspect_ratio_[0] if other_aspect_ratio_ else None
+                fps_ = track.frame_rate
+                fc_ = track.frame_count
+                media_size_1 = track.stream_size
+                other_media_size_ = track.other_stream_size
+                media_size_2 = (
+                    [
+                        other_media_size_[1],
+                        other_media_size_[2],
+                        other_media_size_[3],
+                        other_media_size_[4],
+                    ]
+                    if other_media_size_
+                    else None
+                )
+
+        dict_ = (
+            {
+                "media_type": type_,
+                "format": format_,
+                "duration_in_ms": duration_1,
+                "duration": duration_2,
+                "pixel_sizes": pixel_ratio_,
+                "aspect_ratio_in_fraction": aspect_ratio_1,
+                "aspect_ratio": aspect_ratio_2,
+                "frame_rate": fps_,
+                "frame_count": fc_,
+                "file_size_in_bytes": media_size_1,
+                "file_size": media_size_2,
+            }
+            if found
+            else None
+        )
+        return dict_
+
+
+async def resize_media(media: str, video: bool, fast_forward: bool) -> str:
+    if video:
+        info_ = Media_Info.data(media)
+        width = info_["pixel_sizes"][0]
+        height = info_["pixel_sizes"][1]
+        sec = info_["duration_in_ms"]
+        s = round(float(sec)) / 1000
+
+        if height == width:
+            height, width = 512, 512
+        elif height > width:
+            height, width = 512, -1
+        elif width > height:
+            height, width = -1, 512
+
+        resized_video = f"{media}.webm"
+        if fast_forward:
+            if s > 3:
+                fract_ = 3 / s
+                ff_f = round(fract_, 2)
+                set_pts_ = ff_f - 0.01 if ff_f > fract_ else ff_f
+                cmd_f = f"-filter:v 'setpts={set_pts_}*PTS',scale={width}:{height}"
+            else:
+                cmd_f = f"-filter:v scale={width}:{height}"
+        else:
+            cmd_f = f"-filter:v scale={width}:{height}"
+        fps_ = float(info_["frame_rate"])
+        fps_cmd = "-r 30 " if fps_ > 30 else ""
+        cmd = f"ffmpeg -i {media} {cmd_f} -ss 00:00:00 -to 00:00:03 -an -c:v libvpx-vp9 {fps_cmd}-fs 256K {resized_video}"
+        _, error, __, ___ = await run_cmd(cmd)
+        os.remove(media)
+        return resized_video
+
+    image = Image.open(media)
+    maxsize = 512
+    scale = maxsize / max(image.width, image.height)
+    new_size = (int(image.width * scale), int(image.height * scale))
+
+    image = image.resize(new_size, Image.LANCZOS)
+    resized_photo = "sticker.png"
+    image.save(resized_photo)
+    os.remove(media)
+    return resized_photo
