228db11773e14f70584d3679c03f1daa1fa70a5a
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-27 18:04:00 +0700

    Update profile.py

--- ling/modules/profile.py
+++ ling/modules/profile.py
@@ -1,11 +1,4 @@
-# Credits: @mrismanaziz
-# Copyright (C) 2022 Pyro-ManUserbot
-#
-# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
-#
-# t.me/SharingUserbot & t.me/Lunatic0de
+# Masih Bug Anjing Jan di Ambil
 
 import os
 from asyncio import sleep
